3bcee83d91ff565e48115898e84399134fc61877
UlyssesTranquilino <129757717+UlyssesTranquilino@users.noreply.github.com>
2025-07-25 21:54:53 +0800

    pricing and support

--- src/app/ui/navbar/navbar.tsx
+++ src/app/ui/navbar/navbar.tsx
@@ -2,7 +2,17 @@
 
 import Image from "next/image";
 import { useState } from "react";
-import { Moon, Sun, Menu } from "lucide-react";
+import {
+  Moon,
+  Sun,
+  Menu,
+  BadgeCheck,
+  Sparkles,
+  CreditCard,
+  LifeBuoy,
+  Send,
+  LogOut,
+} from "lucide-react";
 import { useTheme } from "next-themes";
 import Logo from "../../../../public/KeyFlashLogo.png";
 import {
@@ -23,45 +33,91 @@ import Link from "next/link";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import {
   Sheet,
-  SheetTrigger,
+  SheetClose,
   SheetContent,
-  SheetTitle,
   SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
 } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/router";
 import { useAuth } from "@/app/context/AuthContext";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
   const { user, signOut } = useAuth();
   const { setTheme } = useTheme();
   const profile = user?.identities?.[0]?.identity_data?.avatar_url;
   const [isDarkMode, setIsDarkMode] = useState(true);
-  console.log(profile);
+  const pathname = usePathname();
 
   return (
-    <nav className="my-3 px-3 sm:m-3  sm:px-7 sm:py-2 md:px-10 flex justify-between flex-row-reverse items-center w-full">
-      <div className="flex items-center gap-3">
-        {!user && (
+    <nav className="my-3 pl-3 sm:m-3  sm:px-5 sm:py-2 md:px-10 flex justify-between items-center w-full">
+      <div className="flex items-center justify-start gap-10">
+        <Link href="/" className="flex items-center  gap-2 sm:gap-3  ">
+          <Image src={Logo} alt="KeyFlash Logo" className="w-8 sm:w-9 " />
+          <h1 className="font-medium text-base sm:text-lg"> KeyFlash</h1>
+        </Link>
+
+        <div className="hidden lg:flex items-center gap-6">
+          <Link
+            href="/"
+            className={cn(
+              "text-sm text-gray-300 hover:text-white",
+              pathname == "/" && "text-white"
+            )}
+          >
+            Home
+          </Link>
+          <Link
+            href="/contact"
+            className={cn(
+              "text-sm text-gray-300 hover:text-white",
+              pathname == "/contact" && "text-white"
+            )}
+          >
+            Contact
+          </Link>
+          <Link
+            href="/pricing"
+            className={cn(
+              "text-sm text-gray-300 hover:text-white",
+              pathname == "/pricing" && "text-white"
+            )}
+          >
+            Pricing
+          </Link>
+          <Link
+            href="/support"
+            className={cn(
+              "text-sm text-gray-300 hover:text-white",
+              pathname == "/support" && "text-white"
+            )}
+          >
+            Support
+          </Link>
+        </div>
+      </div>
+
+      <div className="flex items-center gap-3 sm:px-3">
+        {user ? (
           <Link
             href="/signin"
-            className="hidden lg:inline-block bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-md p-px text-xs font-semibold leading-6 text-white "
+            className="text-xs lg:text-sm bg-blue-600/10 hover:bg-blue-600/20 border-1 border-blue-500/30 hover:border-blue-400/40 p-[6px] px-3 lg:px-4 rounded-sm"
           >
-            <span className="absolute inset-0 overflow-hidden rounded-md">
-              <span className="absolute inset-0 rounded-md bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
-            </span>
-            <div className="relative flex space-x-2 items-center z-10 rounded-md bg-zinc-950 py-2 px-4 ring-1 ring-white/10">
-              <span>Get Started</span>
-            </div>
-            <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-blue-400/0 via-blue-400/90 to-cyan-400/0 transition-opacity duration-500 group-hover:opacity-40" />
+            Get Started
+          </Link>
+        ) : (
+          <Link
+            href="/dashboard"
+            className="text-xs lg:text-sm bg-blue-600/10 hover:bg-blue-600/20 border-1 border-blue-500/30 hover:border-blue-400/40 p-[6px] px-3 lg:px-4 rounded-sm"
+          >
+            Dashboard
           </Link>
         )}
-        <Link
-          href="/dashboard"
-          className="text-xs lg:text-sm bg-blue-600/10 hover:bg-blue-600/20 border-1 border-blue-500/30 hover:border-blue-400/40 p-[6px] px-3 rounded-sm"
-        >
-          Dashboard
-        </Link>
 
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
@@ -72,24 +128,106 @@ export default function Navbar() {
           <DropdownMenuContent className="w-40 mx-3" align="start">
             <DropdownMenuLabel>My Account</DropdownMenuLabel>
             <DropdownMenuGroup>
-              <DropdownMenuItem>Profile</DropdownMenuItem>
-              <DropdownMenuItem>Billing</DropdownMenuItem>
+              <DropdownMenuItem>
+                <BadgeCheck />
+                Profile
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                {" "}
+                <Sparkles />
+                Upgrade to Pro
+              </DropdownMenuItem>
             </DropdownMenuGroup>
-
             <DropdownMenuSeparator />
-
-            <DropdownMenuItem>Support</DropdownMenuItem>
-            <DropdownMenuItem>Feedback</DropdownMenuItem>
+            <Link href="/feedback">
+              <DropdownMenuItem>
+                <Send />
+                Feedback
+              </DropdownMenuItem>
+            </Link>
             <DropdownMenuSeparator />
-            <DropdownMenuItem>Log out</DropdownMenuItem>
+            <DropdownMenuItem>
+              {" "}
+              <LogOut />
+              Log out
+            </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
-      </div>
 
-      <Link href="/" className="flex items-center  gap-2 sm:gap-3 px-3">
-        <Image src={Logo} alt="KeyFlash Logo" className="w-8 sm:w-9 " />
-        <h1 className="font-medium text-base sm:text-lg"> KeyFlash</h1>
-      </Link>
+        <div className="lg:hidden">
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="ghost" className="cursor-pointer">
+                <Menu />
+              </Button>
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle></SheetTitle>
+                <SheetDescription></SheetDescription>
+              </SheetHeader>
+
+              <div className="flex flex-col gap-3 ">
+                <Link
+                  href="/"
+                  className={cn(
+                    " text-gray-300 hover:text-white p-2 px-6 hover:bg-gray-900/40",
+                    pathname == "/" &&
+                      "text-white bg-blue-600/10 hover:bg-blue-600/10"
+                  )}
+                >
+                  Home
+                </Link>
+                <Link
+                  href="/contact"
+                  className={cn(
+                    " text-gray-300 hover:text-white p-2 px-6 hover:bg-gray-900/40",
+                    pathname == "/contact" &&
+                      "text-white bg-blue-600/10 hover:bg-blue-600/10"
+                  )}
+                >
+                  Contact
+                </Link>
+                <Link
+                  href="/pricing"
+                  className={cn(
+                    " text-gray-300 hover:text-white p-2 px-6 hover:bg-gray-900/40",
+                    pathname == "/pricing" &&
+                      "text-white bg-blue-600/10 hover:bg-blue-600/10"
+                  )}
+                >
+                  Pricing
+                </Link>
+                <Link
+                  href="/support"
+                  className={cn(
+                    " text-gray-300 hover:text-white p-2 px-6 hover:bg-gray-900/40",
+                    pathname == "/support" &&
+                      "text-white bg-blue-600/10 hover:bg-blue-600/10"
+                  )}
+                >
+                  Support
+                </Link>
+              </div>
+              <div className="grid flex-1 auto-rows-min gap-6 px-4"></div>
+              <SheetFooter>
+                <Link
+                  href="/dashboard"
+                  className="text-center  bg-blue-600/10 hover:bg-blue-600/20 border-1 border-blue-500/30 hover:border-blue-400/40 p-[6px] px-3 rounded-sm"
+                >
+                  Dashboard
+                </Link>
+
+                <SheetClose asChild>
+                  <Button variant="outline" className="cursor-pointer">
+                    Close
+                  </Button>
+                </SheetClose>
+              </SheetFooter>
+            </SheetContent>
+          </Sheet>
+        </div>
+      </div>
     </nav>
   );
 }
